cc112c5175f48520857204c55c6e386278801855
Ali Snedden <Ali.Snedden@nationwidechildrens.org>
2022-03-23 22:51:00 -0400

    src/bingo.py : 1. Added read words from a file 2. Removed xy-tick labels 3. Write output to bingo-card.png
    src/encrypt.py :
    1. Sanely initialize encryptD and keyD

--- src/bingo.py
+++ src/bingo.py
@@ -2,29 +2,47 @@
 # Date   : 3/23/22
 # License: MIT
 # Notes  : 
-#   Caesar Cipher : https://en.wikipedia.org/wiki/Caesar_cipher   
-#   https://dadoverflow.com/2019/05/31/python-bingo/
+#   Caesar Cipher   : https://en.wikipedia.org/wiki/Caesar_cipher   
+#   For inspiration : https://dadoverflow.com/2019/05/31/python-bingo/
+#
 # Background : 
 #       
 # Purpose :
 #   Generates encrypted words
 #   
-#  
 #
 # How to Run :
+#       python3 src/bingo.py wordlist.txt
 import sys
 from matplotlib import pyplot as plt
 import matplotlib.pyplot as plt
-#import matplotlib.style as style
 import numpy as np
 import random
  
 from error import exit_with_error
 from error import warning
-#%matplotlib inline
-#style.use('seaborn-poster')
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 20})
+
+
+def print_help(Arg):
+    """
+    ARGS:
+        Arg     : int, exit value
+    DESCRIPTION:
+        Print Help. Exit with value arg
+    RETURN:
+        N/A
+    DEBUG:
+        1. Tested, it worked
+    FUTURE:
+    """
+    sys.stdout.write(
+        "\nUSAGE : python src/bingo.py wordlist.txt \n\n"
+        "   wordlist.txt : (file) List of words (one entry per line) to make bingo out of\n"
+        "                         \n")
+    sys.exit(Arg)
+
 
 
 def main(Verbose=True):
@@ -36,25 +54,29 @@ def main(Verbose=True):
     DEBUG:
     FUTURE:
     """
-    wordL = ["cat", "hat", "pop", "mom", "dad", "hog", "girl", "boy", "dirt", "red", "rat",
-              "blue", "tree", "code", "run", "leaf", "tent", "camp", "help", "trust", "friend",
-              "strong", "respect", "rain", "fun", "sun", "swim", "team", "program", "doctor"]
-
+    path = sys.argv[1]
+    fin  = open(path, "r")
+    wordL=[]
+    for line in fin:
+        wordL.append(line)
+    
     random.shuffle(wordL)
-    rowlen= 4  # make any size card you'd like
+    nRow= 4                     # make any size card you'd like
      
     fig = plt.figure()
     ax = fig.gca()
-    ax.set_xticks(np.arange(0, rowlen + 1))
-    ax.set_yticks(np.arange(0, rowlen + 1))
+    ax.set_xticks(np.arange(0, nRow + 1))
+    ax.set_yticks(np.arange(0, nRow + 1))
+    ax.xaxis.set_ticklabels([])
+    ax.yaxis.set_ticklabels([])
     plt.grid()
      
-    for i, word in enumerate(wordL[:rowlen**2]):
-        x = (i % rowlen) + 0.4
-        y = int(i / rowlen) + 0.5
+    # Create bingo plot - Grab only enough elements to fill it
+    for i, word in enumerate(wordL[:nRow**2]):
+        x = (i % nRow) + 0.25
+        y = int(i / nRow) + 0.25
         ax.annotate(word, xy=(x, y), xytext=(x, y))
-         
-    plt.show()
+    plt.savefig("bingo-card.png")
 
 
 if __name__ == "__main__":
